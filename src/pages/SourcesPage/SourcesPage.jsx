01aabe61d33d7feeb5477ab20780b6f462e837bc
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2021-05-01 17:38:03 -0700

    add loader; attach to pages

--- src/pages/SourcesPage/SourcesPage.jsx
+++ src/pages/SourcesPage/SourcesPage.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import PageWrapper from '../../components/PageWrapper/PageWrapper'
+import React, {useEffect, useState} from 'react'
+import {PageWrapper} from '../../components/PageWrapper/PageWrapper'
 import InfoSection from '../../components/InfoSection/InfoSection'
+import Loader from '../../components/Loader/Loader'
 
 import {sectionData} from './Data'
 
-function SourcesPage(){
-  return(
-    <PageWrapper>
-      {
-        sectionData.map(oneSection => {
-          return(<InfoSection {...oneSection}></InfoSection>);
-        })
+
+// initialLoading:
+// Set to '1' to skip loader animation.
+function SourcesPage(initialLoading = 0){
+  const [timesLoaded, setLoading] = useState(initialLoading);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (timesLoaded !== 1){
+        setLoading(1);
+        //console.log("Set 'timesLoaded' to 1!", timesLoaded)
       }
-    </PageWrapper>
+    }, 2150)
+  })
+
+  return(
+    <>
+      
+      {
+        (timesLoaded === 1) ? 
+          
+          <PageWrapper>
+            {
+              sectionData.map(oneSection => {
+                return(<InfoSection {...oneSection}></InfoSection>);
+              })
+            }
+          </PageWrapper>
+
+        :
+          <Loader></Loader>
+      }
+      
+    </>
   )
 }
 
